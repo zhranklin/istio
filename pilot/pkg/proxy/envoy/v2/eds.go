354f3b38b3dd7f59ba94c46a98471388fe580abf
yangdihang <ivanyangtt@zju.edu.cn>
2019-05-06 16:31:02 +0800

    Enhance: Add backup strategy
    The backup address usually points to the gateway address. When there is no access to the upstream service in the cluster, the backup address is used to access the out-of-cluster upstream service.

--- pilot/pkg/proxy/envoy/v2/eds.go
+++ pilot/pkg/proxy/envoy/v2/eds.go
@@ -17,6 +17,7 @@ package v2
 import (
 	"reflect"
 	"strconv"
+	"strings"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -678,8 +679,45 @@ func (s *DiscoveryServer) loadAssignmentsForClusterLegacy(push *model.PushContex
 		l = loadAssignment(c)
 	}
 
+	if s.Env.BackupAddress != "" {
+		if ok, address, port := checkAddress(s.Env.BackupAddress); ok {
+			host := util.BuildAddress(address, uint32(port))
+			ep := endpoint.LbEndpoint{
+				HostIdentifier: &endpoint.LbEndpoint_Endpoint{
+					Endpoint: &endpoint.Endpoint{
+						Address: &host,
+					},
+				},
+				LoadBalancingWeight: &types.UInt32Value{
+					Value: 1,
+				},
+			}
+			eps := []endpoint.LbEndpoint{ep}
+			l.Endpoints = append(l.Endpoints, endpoint.LocalityLbEndpoints{
+				LbEndpoints: eps,
+				LoadBalancingWeight: &types.UInt32Value{
+					Value: 1,
+				},
+				Priority: 1,
+			})
+		} else {
+			s.Env.BackupAddress = ""
+		}
+	}
 	return l, true
+}
 
+func checkAddress(address string) (bool, string, int) {
+	strs := strings.Split(address, ":")
+	if len(strs) != 2 {
+		adsLog.Errorf("BackupAddress:%s is invaild , case missing port", address)
+		return false, "", -1
+	}
+	port, err := strconv.Atoi(strs[1])
+	if err != nil {
+		adsLog.Errorf("BackupAddress:%s is invaild, case:%v  ", address, err)
+	}
+	return true, strs[0], port
 }
 
 // loadAssignmentsForClusterIsolated return the endpoints for a proxy in an isolated namespace
