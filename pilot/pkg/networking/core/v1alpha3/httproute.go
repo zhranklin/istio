7ee8bcec9ecf5eed0cad260071fe8e3aa479e029
yangdihang <ivanyangtt@zju.edu.cn>
2019-08-01 21:25:05 +0800

    Fix: Duplicate domain when add prefix and suffix

--- pilot/pkg/networking/core/v1alpha3/httproute.go
+++ pilot/pkg/networking/core/v1alpha3/httproute.go
@@ -182,6 +182,12 @@ func (configgen *ConfigGeneratorImpl) buildSidecarOutboundHTTPRouteConfig(env *m
 				}
 				if (push.Env.NsfHostPrefix != "" || push.Env.NsfHostSuffix != "") && isK8SSvcHost(host) {
 					serviceName := strings.Split(host, ".")[0]
+					n := fmt.Sprintf("%s:%d", push.Env.NsfHostPrefix+serviceName+push.Env.NsfHostSuffix, virtualHostWrapper.Port)
+					if _, ok := uniques[n]; ok {
+						push.Add(model.DuplicatedDomains, name, node, fmt.Sprintf("duplicate domain from virtual service: %s, when add prefix and suffix", name))
+						log.Debugf("Dropping duplicate route entry %v.", n)
+						continue
+					}
 					vh.Domains = append(vh.Domains, push.Env.NsfHostPrefix+serviceName+push.Env.NsfHostSuffix)
 				}
 				virtualHosts = append(virtualHosts, vh)
@@ -202,6 +208,12 @@ func (configgen *ConfigGeneratorImpl) buildSidecarOutboundHTTPRouteConfig(env *m
 				}
 				if (push.Env.NsfHostPrefix != "" || push.Env.NsfHostSuffix != "") && isK8SSvcHost(string(svc.Hostname)) {
 					serviceName := strings.Split(string(svc.Hostname), ".")[0]
+					n := fmt.Sprintf("%s:%d", push.Env.NsfHostPrefix+serviceName+push.Env.NsfHostSuffix, virtualHostWrapper.Port)
+					if _, ok := uniques[n]; ok {
+						push.Add(model.DuplicatedDomains, name, node, fmt.Sprintf("duplicate domain from virtual service: %s, when add prefix and suffix", name))
+						log.Debugf("Dropping duplicate route entry %v.", n)
+						continue
+					}
 					vh.Domains = append(vh.Domains, push.Env.NsfHostPrefix+serviceName+push.Env.NsfHostSuffix)
 				}
 				virtualHosts = append(virtualHosts, vh)
